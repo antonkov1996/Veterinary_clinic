f1cf59d9aaa9e265beaad3d9bce29dbab822413c
antonkov1996 <antonkov1996@gmail.com>
2017-08-15 11:32:23 +0300

    New method

--- src/main/java/ru/clinic/Main.java
+++ src/main/java/ru/clinic/Main.java
@@ -16,36 +16,59 @@ public class Main {
      *
      * @param args
      */
+    public static Vector<Order> orderVector = new Vector<>();
+
     public static void main(String[] args) {
-        Vector<Visitor> visitorVector =new Vector<>();
-        Vector<Veterinary> veterinaryVector =new Vector<>();
-        Veterinary veterinary1 = new Veterinary("Nick","Daniels", Veterinary.Skill.intern);
-        Veterinary veterinary2 = new Veterinary("Thomas","Brown", Veterinary.Skill.doctor);
-        Veterinary veterinary3 = new Veterinary("James","Evans", Veterinary.Skill.director);
+        Vector<Visitor> visitorVector = new Vector<>();
+        Vector<Veterinary> veterinaryVector = new Vector<>();
+        Veterinary veterinary1 = new Veterinary("Nick", "Daniels", Veterinary.Skill.intern);
+        Veterinary veterinary2 = new Veterinary("Thomas", "Brown", Veterinary.Skill.doctor);
+        Veterinary veterinary3 = new Veterinary("James", "Evans", Veterinary.Skill.director);
         veterinaryVector.add(veterinary1);
         veterinaryVector.add(veterinary2);
         veterinaryVector.add(veterinary3);
 
         for (int i = 0; i < 10; i++) {
             Random rnd = new Random();
-            Pet pet = new Pet(illness.PetType[rnd.nextInt(3)],illness.names[rnd.nextInt(9)],rnd.nextInt(5),rnd.nextInt(10),false,illness.IllnesType[rnd.nextInt(3)]);
-            Visitor visitor = new Visitor(illness.names[rnd.nextInt(9)], illness.surnames[rnd.nextInt(9)],pet);
+            Pet pet = new Pet(illness.PetType[rnd.nextInt(3)], illness.names[rnd.nextInt(9)], rnd.nextInt(5), rnd.nextInt(10), false, illness.IllnesType[rnd.nextInt(3)]);
+            Visitor visitor = new Visitor(illness.names[rnd.nextInt(9)], illness.surnames[rnd.nextInt(9)], pet);
             visitorVector.add(visitor);
         }
-        for (Visitor visitors:visitorVector
-                ) {
-            System.out.println("Name: "+visitors.getName()+" Surname: "+visitors.getSurname()+" with pet: "+visitors.getPet().getName()+" age: "+visitors.getPet().getAge()+" illness: "+visitors.getPet().getIllnesType());
-
-        }
-        System.out.println("Doctors vector: ");
-        for (Veterinary veterinaris:veterinaryVector
-             ) {
-            System.out.println("Name: "+veterinaris.getName()+" Surname: "+veterinaris.getSurname()+" with skill: "+veterinaris.getSkill());
-        }
-
 
 
     }
 
+    /**
+     * Method print all persons in vector
+     *
+     * @param veterinaryVector vector with veterinary
+     */
+    private void veterinaryMonitor(Vector<Veterinary> veterinaryVector) {
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Veterinary vector");
+        for (Veterinary veterinaris : veterinaryVector
+                ) {
+            System.out.println("Name: " + veterinaris.getName() + " Surname: " + veterinaris.getSurname() + " with skill: " + veterinaris.getSkill());
+        }
+    }
 
+    /**
+     * Method print all persons in vector
+     *
+     * @param visitor
+     */
+    private void vititorMonitor(Vector<Visitor> visitor) {
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Visitor vector");
+        for (int i = 0; i < visitor.size(); i++) {
+            for (Visitor visitors : visitor
+                    ) {
+
+                System.out.println("Name: " + visitors.getName() + " Surname: " + visitors.getSurname() + " with pet: " + visitors.getPet().getName() + " age: " + visitors.getPet().getAge() + " illness: " + visitors.getPet().getIllnesType());
+
+            }
+        }
+
+
+    }
 }
